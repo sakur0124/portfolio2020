4b5c65a251a12667b6b3122ec304c9b1858c5583
sakur0124 <56106575+sakur0124@users.noreply.github.com>
2020-07-05 17:34:13 +0900

    Add files via upload

---
+++ js/flower.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{   
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > $('.peoplePicture img').offset().top - $(window).height()/2 ){
+            $('.peoplePicture').addClass('toShow');
+            $('.text p').addClass('toShow');
+        }
+    });
+
+    $(window).on('scroll',function(){
+           if( $(window).scrollTop() > $('footer').offset().top - $(window).height()){
+            $('footer .sns li').each(function(i){
+                $(this).delay(i * 300).queue(function(next){
+                    $(this).addClass('toShow');
+                    next();
+                });
+            });
+            $('.opHours').addClass('toShow');
+
+           }
+          
+        
+       });
+
+
+}
