3a005f25d107d648f2de282511ca130cf7c35171
sakur0124 <56106575+sakur0124@users.noreply.github.com>
2020-07-07 19:21:09 +0900

    Add files via upload

--- js/flower.js
+++ js/flower.js
@@ -1,27 +1,33 @@
-'use strict';
+"use strict";
 
-{   
-    $(window).on('scroll',function(){
-        if($(window).scrollTop() > $('.peoplePicture img').offset().top - $(window).height()/2 ){
-            $('.peoplePicture').addClass('toShow');
-            $('.text p').addClass('toShow');
-        }
-    });
+{
+  $(window).on("scroll", function () {
+    if (
+      $(window).scrollTop() >
+      $(".peoplePicture img").offset().top - $(window).height() / 2
+    ) {
+      $(".peoplePicture").addClass("toShow");
+      $(".text p").addClass("toShow");
+    }
+  });
 
-    $(window).on('scroll',function(){
-           if( $(window).scrollTop() > $('footer').offset().top - $(window).height()){
-            $('footer .sns li').each(function(i){
-                $(this).delay(i * 300).queue(function(next){
-                    $(this).addClass('toShow');
-                    next();
-                });
-            });
-            $('.opHours').addClass('toShow');
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > $("footer").offset().top - $(window).height()) {
+      $("footer .sns li").each(function (i) {
+        $(this)
+          .delay(i * 300)
+          .queue(function (next) {
+            $(this).addClass("toShow");
+            next();
+          });
+      });
+      $(".opHours").addClass("toShow");
+    }
+  });
 
-           }
-          
-        
-       });
-
-
+  $(".toHide").on("click", function () {
+    // $(".mainContent").toggleClass("toShow");
+    $(".hideContent").toggleClass("toShow");
+    $(".toHide").toggleClass("toMove");
+  });
 }
