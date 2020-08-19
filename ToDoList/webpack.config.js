74860cb1ff8dead782a6a0b4ca2d95a35fff6ba8
sakur0124 <56106575+sakur0124@users.noreply.github.com>
2020-08-19 16:21:55 +0900

    Add files via upload
    add todolist

---
+++ ToDoList/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+module.exports = {
+  entry: ["./index.js", "./app.jsx"],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./"),
+  },
+  module: {
+    rules: [
+      //第一個loader編譯JSX
+      {
+        test: /.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-react"] },
+        },
+      },
+      //第二個loader編譯ES6
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+    ],
+  },
+};
